d5ea46199815c1f9772542639e9620be57883dc7
Shalinda Ayeshmantha <Shalinda.Silva@cloudsolutions.com.sa>
2022-07-03 15:55:13 +0530

    form done

--- predictionForm/src/app/app.component.ts
+++ predictionForm/src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'predictionForm';
+  predictionForm = new FormGroup({
+    swt: new FormControl(false, { validators: [Validators.required]}),
+    diffBr: new FormControl(false, { validators: [Validators.required]}),
+    chestPn: new FormControl(false, { validators: [Validators.required]}),
+    abCramp: new FormControl(false, { validators: [Validators.required]}),
+    headache: new FormControl(false, { validators: [Validators.required]}),
+    dizz: new FormControl(false, { validators: [Validators.required]}),
+    fear: new FormControl(false, { validators: [Validators.required]}),
+    depres: new FormControl(false, { validators: [Validators.required]}),
+    flashbk: new FormControl(false, { validators: [Validators.required]}),
+    trem: new FormControl(false, { validators: [Validators.required]}),
+    bluVi: new FormControl(false, { validators: [Validators.required]}),
+    tunVi: new FormControl(false, { validators: [Validators.required]}),
+    senTer: new FormControl(false, { validators: [Validators.required]}),
+    firstYes: new FormControl(false, { validators: [Validators.required]}),
+    lastYes: new FormControl(false, { validators: [Validators.required]}),
+  });
+
+  isLoading: boolean = true;
+  formData: any;
+
+  ngOnInit(): void {
+    this.isLoading = false;
+  }
+  onSubmit(){
+    this.formData = this.predictionForm.getRawValue();
+  }
 }
--- predictionForm/src/app/app.module.ts
+++ predictionForm/src/app/app.module.ts
@@ -3,6 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 @NgModule({
   declarations: [
@@ -10,7 +20,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatRadioModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
